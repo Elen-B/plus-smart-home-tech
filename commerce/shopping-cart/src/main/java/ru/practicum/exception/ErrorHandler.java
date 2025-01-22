94c71e93d074f4ba975969436b47939f3be5e889
Elen-B <beskrovnaia.elena@yandex.ru>
2025-01-22 11:18:47 +0500

    fix responce httpstatus to UNAUTHORIZED (401) for null username in shopping cart module

--- commerce/shopping-cart/src/main/java/ru/practicum/exception/ErrorHandler.java
+++ commerce/shopping-cart/src/main/java/ru/practicum/exception/ErrorHandler.java
@@ -1,7 +1,9 @@
 package ru.practicum.exception;
 
 import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -53,16 +55,33 @@ public class ErrorHandler {
     }
 
     @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleConstraintViolationException(final ConstraintViolationException e) {
-        return new ErrorResponse(
-                e.getCause(),
-                e.getStackTrace(),
-                HttpStatus.BAD_REQUEST,
-                e.getMessage(),
-                e.getMessage(),
-                e.getSuppressed(),
-                e.getLocalizedMessage()
+    public ResponseEntity<ErrorResponse> handleConstraintViolationException(final ConstraintViolationException e) throws NotAuthorizedUserException {
+        if (e.getConstraintViolations().stream()
+                .anyMatch(violation -> violation.getConstraintDescriptor()
+                        .getAnnotation().annotationType().equals(NotBlank.class))) {
+            return new ResponseEntity<>(
+                    new ErrorResponse(
+                            e.getCause(),
+                            e.getStackTrace(),
+                            HttpStatus.UNAUTHORIZED,
+                            e.getMessage(),
+                            e.getMessage(),
+                            e.getSuppressed(),
+                            e.getLocalizedMessage()
+                    ), HttpStatus.UNAUTHORIZED
+            );
+        }
+
+        return new ResponseEntity<>(
+                new ErrorResponse(
+                        e.getCause(),
+                        e.getStackTrace(),
+                        HttpStatus.BAD_REQUEST,
+                        e.getMessage(),
+                        e.getMessage(),
+                        e.getSuppressed(),
+                        e.getLocalizedMessage()
+                ), HttpStatus.BAD_REQUEST
         );
     }
 }
